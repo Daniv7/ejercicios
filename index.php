bd4161f4f72147aae1fc6dc97e92fa6966583310
Danilo Valladares <89943738+Daniv7@users.noreply.github.com>
2022-04-19 14:48:32 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<?php
+
+// Variables
+
+$alumnos = "Alumnos por favor sentarse en el pupitre y guardar silencio.";
+$profesores = "Si, ya vamos Prof. Peter";
+
+echo "$alumnos" . "$profesores";
+
+?>
+
+
+
+
+
+
+
+
+
